419b29d6a46c78be655b9ecc4dd2751c2b0b83bb
Michael Bright <github.com@mjbright.net>
2018-10-02 19:06:07 +0200

    Reimplemented REDEPLOY.sh to create user(ns) specific kubelab yaml from template

--- examples/notebooks/docker/REDEPLOY.sh
+++ examples/notebooks/docker/REDEPLOY.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+YAML_BASE=kubelab
+TEMPLATE=${YAML_BASE}.yml.TEMPLATE
+
+BASE_PORT=8800
+
+USERS=1
+
+
 # Function: press <prompt>
 # Prompt user to press <return> to continue
 # Exit if the user enters q or Q
@@ -30,9 +38,25 @@ die() {
     exit 1
 }
 
+CREATE_YAML() {
+    NAMESPACE=$1; shift
+    USER=$1; shift
+    YAML_FILE=$1; shift
+
+    let PUBLIC_PORT=BASE_PORT+USER
+
+    sed -e "s/PUBLIC_PORT/$PUBLIC_PORT/g" \
+        -e "s/NAMESPACE/$NAMESPACE/g" \
+        < $TEMPLATE > $YAML_FILE
+    ls -altr $YAML_FILE
+}
+
 RESTART_JUPYTER() {
-    RUN kubectl delete -f jupyter.yml
-    RUN kubectl apply -f jupyter.yml 
+    YAML_FILE=$1; shift
+
+    RUN kubectl delete -f $YAML_FILE
+    RUN kubectl apply -f $YAML_FILE 
+
     RUN kubectl get pods jupyter
 
     echo "Waiting for Pod to start ..."
@@ -42,12 +66,26 @@ RESTART_JUPYTER() {
 }
 
 GET_JUPYTER_TOKEN_URL() {
+    TOKEN_FILE=$1; shift
+
     echo "Getting NODE/IP/PORT for Jupyter ..."
     NODE=$(kubectl get --no-headers pods jupyter -o wide | awk '{ print $NF; }')
     [ -z "$NODE" ] && die "Failed to get NODE"
     echo "Running on node <$NODE>"
 
-    NODE_IP=$(~/z/bin/win64/doctl.exe compute droplet list | awk "/ $NODE / { print \$3; }")
+    case $NODE in
+        docker-for-desktop) NODE_IP=127.0.0.1;;
+
+        *) 
+          # TO CHECK ON digital-ocean (my tf, or their managed):
+          # TO CHECK ON azure/aks:
+          NODE_IP=$(kubectl describe nodes $NODE | awk '/InternalIP:/ { print $2; }')
+          ;;
+
+    esac
+
+    #Digital Ocean:
+    #NODE_IP=$(~/z/bin/win64/doctl.exe compute droplet list | awk "/ $NODE / { print \$3; }")
     [ -z "$NODE_IP" ] && die "Failed to get NODE_IP"
     echo "Node <$NODE> has IP <$NODE_IP>"
 
@@ -63,19 +101,42 @@ GET_JUPYTER_TOKEN_URL() {
 
     TOKEN_URL="$URL/?token=$TOKEN"
 
-    echo
-    echo "Jupyter address is $URL"
-    echo
+    ## echo
+    ## echo "Jupyter address is $URL"
+    ## echo
     echo "******** Connect using $TOKEN_URL"
-
-    echo
-    echo "Using Docker image:"
-    kubectl describe pod jupyter | grep -i docker
+    ## echo
+    ## echo "Using Docker image:"
+    ## kubectl describe pod jupyter | grep -i docker
 
     RUN kubectl logs -f jupyter
+
+    echo "$TOKEN_URL" > $TOKEN_FILE
+    ls -altr $TOKEN_FILE
 }
 
-RESTART_JUPYTER
-GET_JUPYTER_TOKEN_URL
+## -- Args: ----------------------------------------------------------------------
+
+while [ ! -z "$1" ]; do
+    case $1 in
+        -[0-9]*) USERS=${1#-}; break;;
+        [0-9]*)  USERS=$1; break;;
+        *)      die "Unknown option <$1>";;
+    esac
+    shift
+done
+
+## -- Main: ----------------------------------------------------------------------
+
+for USER in $(seq 1 $USERS); do
+    NAMESPACE=USER$USER
+    echo USER$USER
+    kubectl create namespace $NAMESPACE
+    CREATE_YAML $NAMESPACE $USER kubelab_user$USER.yml
+    RESTART_JUPYTER kubelab_user$USER.yml
+    GET_JUPYTER_TOKEN_URL kubelab_user$USER.txt
+done
+exit 0
+
 
 
